5f60dfe3de87c63fc3235c04014926262fee7eba
мансур зурабов <asl.zurabov@gmail.com>
2022-09-15 21:41:43 +0300

    color flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,10 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", 'gray', 'yellow', 'orange'];
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function(){
+    // get random number between 0-3
+    const randomNumber = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
