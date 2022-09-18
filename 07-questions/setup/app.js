e9e2e5d6e1936015f9e7ab1bd0bed3e13fa7b102
мансур зурабов <asl.zurabov@gmail.com>
2022-09-18 20:47:40 +0300

    question project second option

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,11 +1,30 @@
 //using selectors inside the element
+
+
+const questions = document.querySelectorAll('.question')
+
+questions.forEach(function (question){
+    const btns = question.querySelectorAll('.question-btn')
+    btns.forEach(function(btn){
+        btn.addEventListener('click', function(){
+            question.classList.toggle('show-text')
+        })
+    })
+    })
+
+
+
+
+
+
+
 // traversing the dom
 
 
-const btns = document.querySelectorAll('.question-btn')
+// const btns = document.querySelectorAll('.question-btn')
 
-btns.forEach(function(btn){
-    btn.addEventListener('click',function(e){
-    e.currentTarget.parentElement.parentElement.classList.toggle('show-text')
-    })
-})
+// btns.forEach(function(btn){
+//     btn.addEventListener('click',function(e){
+//     e.currentTarget.parentElement.parentElement.classList.toggle('show-text')
+//     })
+// })
