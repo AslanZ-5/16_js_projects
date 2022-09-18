c950dced51330ea99b324b2d54e41a24cbe12657
мансур зурабов <asl.zurabov@gmail.com>
2022-09-18 12:10:52 +0300

    emplementing toggle with if else statement

--- 05-sidebar/setup/app.js
+++ 05-sidebar/setup/app.js
@@ -2,3 +2,12 @@ const togglebtn = document.querySelector('.sidebar-toggle')
 const closeBtn = document.querySelector('.close-btn')
 const sidebar = document.querySelector('.sidebar')
 
+togglebtn.addEventListener('click', function(){
+    if (sidebar.classList.contains('show-sidebar')){
+        sidebar.classList.remove('show-sidebar')
+
+    }
+    else{
+        sidebar.classList.add('show-sidebar')
+    }
+})
